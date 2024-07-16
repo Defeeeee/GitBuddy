9f8d1b9ac2585040acd8d8c3df325039906f01b2
Federico Diaz Nemeth <81602924+Defeeeee@users.noreply.github.com>
2024-07-15 22:28:15 -0300

    feat: Added adjustment to system time

--- config.py
+++ config.py
@@ -76,6 +76,7 @@ def adjust_cron_schedule_to_system_timezone(cron_schedule, user_timezone):
     """Adjusts the cron schedule based on the system's and user's timezones."""
     try:
         system_timezone_str = subprocess.check_output(["date", "+%z"]).decode().strip()  # Get system timezone
+        print(f"System timezone: {system_timezone_str}")
         system_timezone = timezone(timedelta(hours=int(system_timezone_str[:1]), minutes=int(system_timezone_str[3:])))
     except (subprocess.CalledProcessError, ValueError) as e:
         print(f"Error getting or parsing system timezone: {e}. Using unadjusted schedule.")
